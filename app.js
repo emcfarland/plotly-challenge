3ab9fc46b893f7733fc10dfcc7478624c41f1fb8
unknown <elliottmcf@gmail.com>
2021-01-09 11:39:43 -0500

    Reads json file and prints metadata to console

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
-/* The following is an example on how you might structure your code.
-This is not the only way to complete this assignment.
-Feel free to disregard and create your own code */
-
 // Define a function that will create metadata for given sample
 function buildMetadata(sample) {
 
     // Read the json data
-
+    d3.json("data/samples.json").then(function(d) {
         // Parse and filter the data to get the sample's metadata
-
+        var metadata = d.metadata[0];
+        console.log(metadata);
         // Specify the location of the metadata and update it
-
+    });
 }
-
 // Define a function that will create charts for given sample
 function buildCharts(sample) {
 
@@ -43,11 +39,14 @@ function init() {
 function optionChanged(newSample){
 
     // Update metadata with newly selected sample
-
+    var dropdownMenu = d3.select("#selDataset");
+    // Assign the value of the dropdown menu option to a variable
+    var dataset = dropdownMenu.property("value");
     // Update charts with newly selected sample
 
 }
 
 // Initialize dashboard on page load
-init();
+//init();
 
+buildMetadata();
