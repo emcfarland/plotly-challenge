0306c5bdf04f4d595c052fffc25bd93b63808a20
unknown <elliottmcf@gmail.com>
2021-01-09 22:55:38 -0500

    Corrects bar chart labels

--- app.js
+++ app.js
@@ -29,22 +29,43 @@ function buildCharts(sample) {
         // console.log(sampleDataset);
 
         var sampleValues = sampleDataset[0].sample_values;
-        var otuIDs = sampleDataset[0].otu_ids;
+        var otuIDStrings = [];
+
+        var otuIDs = sampleDataset[0].otu_ids
+        otuIDs.forEach(id => otuIDStrings.push(`OTU ${id}`));
         var otuLabels = sampleDataset[0].otu_labels;
 
         // Bar
         var barData = [{
             type: 'bar',
-            x: sampleValues.slice(0,10),
-            y: otuIDs.slice(0,10),
-            text: otuLabels.slice(0,10),
+            x: sampleValues.slice(0,10).reverse(),
+            y: otuIDStrings.slice(0,10).reverse(),
+            text: otuLabels.slice(0,10).reverse(),
             orientation: 'h'
         }];
           
         Plotly.newPlot("bar", barData);
 
         // Bubble Chart
-        var bubbleData;
+        var bubbleData = [{
+            x: otuIDs,
+            y: sampleValues,
+            text: otuLabels,
+            mode: 'markers',
+            marker: {
+              color: otuIDs,
+              size: sampleValues
+            }
+          }];
+          
+          var bubbleLayout = {
+            title: 'Belly Button Biodiversity Bubble Chart',
+            showlegend: false,
+            height: 600,
+            width: 1000
+          };
+          
+          Plotly.newPlot('bubble', bubbleData, bubbleLayout);
         
 
     });
@@ -84,11 +105,29 @@ function init() {
 
 function optionChanged(newSample){
 
-    // console.log(newSample);
     // Update metadata with newly selected sample
     buildMetadata(newSample);
     // Update charts with newly selected sample
+    d3.json("data/samples.json").then(function(data) {
+        var dataset = data.samples;
+        var sampleDataset = dataset.filter(d => parseInt(d.id) === parseInt(newSample));
+        // console.log(sampleDataset);
 
+        var sampleValues = sampleDataset[0].sample_values;
+        var otuIDStrings = [];
+
+        var otuIDs = sampleDataset[0].otu_ids
+        otuIDs.forEach(id => otuIDStrings.push(`OTU ${id}`));
+        var otuLabels = sampleDataset[0].otu_labels;
+
+        var barUpdate = {
+            x: [sampleValues.slice(0,10).reverse()],
+            y: [otuIDStrings.slice(0,10).reverse()],
+            text: [otuLabels.slice(0,10).reverse()],
+        };
+          
+        Plotly.restyle("bar", barUpdate);
+    });
 }
 
 // Initialize dashboard on page load
